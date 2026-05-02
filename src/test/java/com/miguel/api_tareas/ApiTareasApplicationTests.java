a4e0e638300b7c921b7c2279877251df4e0d4a87
miferdev <fdezguerrerom@gmail.com>
2026-05-02 20:25:46 +0200

    Creacion del modelo de la tarea

---
+++ src/test/java/com/miguel/api_tareas/ApiTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.api_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
